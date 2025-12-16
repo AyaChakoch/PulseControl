5be031d0048e5264e50a6e301cf7397bf9602e8d
AyaChakoch <aya98chakoch@gmail.com>
2025-12-16 17:00:17 +0100

    Initial Commit

---
+++ PulseControl/PulseControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseControlApp.swift
+//  PulseControl
+//
+//  Created by Aya on 2025-12-16.
+//
+
+import SwiftUI
+
+@main
+struct PulseControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
